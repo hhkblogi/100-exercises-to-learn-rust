8062a60261bc7e4d1753b451ac193d25720edcb9
hhkb logi <hhkb.logi@gmail.com>
2024-09-15 19:29:51 +0100

    Update solution

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,12 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    // todo!()
+    // let mut n2 = n.clone();
+    let mut p: u32 = 1;
+    for i in 1..=n {
+        p *= i;
+    }
+    p
 }
 
 #[cfg(test)]
